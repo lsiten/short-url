b142a3000dcdbbc9a9a168abd2ba8beafbe4fdda
雷诗城 <leishicheng@MacBook-Pro-3.local>
2021-03-15 14:29:52 +0800

    fix: 删除多余url

--- app/router.js
+++ app/router.js
@@ -5,9 +5,6 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
-  router.get('/eat/index', controller.eat.index);
-  router.get('/eat/getrestaurant', controller.eat.getrestaurant);
   router.get('/short/:key', controller.url.redirectUrl);
   router.get('/url/create', controller.url.genShortUrl);
 };
